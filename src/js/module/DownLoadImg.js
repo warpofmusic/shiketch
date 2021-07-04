58ce2d4362567fd59e76509284b33a2525af5cc0
しいもと <warp.of.music@gmail.com>
2021-07-05 00:43:30 +0900

    ペーストに関する技術調査終了

--- src/js/module/DownLoadImg.js
+++ src/js/module/DownLoadImg.js
@@ -1,15 +1,17 @@
 let DownLoadImg = {
 	shiketch : {},
+	board : {},
 	run:function(){
-		html2canvas(DownLoadImg.shiketch).then(canvas => { 
+		html2canvas(DownLoadImg.board).then(canvas => { 
 	       let downloadEle = document.createElement("a");
 	       downloadEle.href = canvas.toDataURL("image/png");
 	       downloadEle.download = "shiketchImg.png";
 	       downloadEle.click();
 	   });
 	},
-	eventListener:function(shiketch){
+	eventListener:function(shiketch,board){
 		DownLoadImg.shiketch = shiketch;
+		DownLoadImg.board = board;
 		let btn = document.getElementById("btnExportImg");
 		btn.addEventListener("click",() => {
 			DownLoadImg.run();
